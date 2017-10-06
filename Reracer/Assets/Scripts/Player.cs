940859de9cd71ac119115615f2e57e090a245c4d
Rernokk <ssmith37@uccs.edu>
2017-10-05 23:22:35 -0600

    Update

--- Reracer/Assets/Scripts/Player.cs
+++ Reracer/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Player : NetworkBehaviour
 {
@@ -9,19 +10,28 @@ public class Player : NetworkBehaviour
   public float Speed;
   public GameObject myCamera;
   public GameObject[] myPickups;
+  Image HealthBar;
+  int originalSteeringValue = 135, originalSpeedLimit = 60;
+
+  [SyncVar]
+  bool Boosting = false, Blocking = false;
+
+  [SyncVar]
+  public bool CatBlocked = false;
 
   [SyncVar]
   public GameObject Pickup, Obstacle;
 
   [SerializeField]
   [SyncVar]
-  int myHealth = 100;
+  int myHealth = 100, catCounter = 0;
 
   [SyncVar]
   bool dead = false;
 
   [SyncVar]
   public string myPickup;
+  GameObject myShield;
 
   public int Health
   {
@@ -36,6 +46,7 @@ public class Player : NetworkBehaviour
     rgd2d = GetComponent<Rigidbody2D>();
     myCamera = Instantiate(myCamera, new Vector3(transform.position.x, transform.position.y, -20), Quaternion.identity);
     myCamera.GetComponent<CameraScript>().Player = transform;
+    HealthBar = myCamera.transform.Find("Canvas/Health").GetComponent<Image>();
   }
   void Update()
   {
@@ -43,6 +54,7 @@ public class Player : NetworkBehaviour
     {
       return;
     }
+    HealthBar.material.SetFloat("_Value", (float)Health / 100);
 
     if (dead)
     {
@@ -64,11 +76,19 @@ public class Player : NetworkBehaviour
     if (Input.GetKey(KeyCode.A))
     {
       transform.Rotate(new Vector3(0, 0, 135) * Time.deltaTime);
+      if (CatBlocked)
+      {
+        catCounter++;
+      }
     }
 
     if (Input.GetKey(KeyCode.D))
     {
       transform.Rotate(new Vector3(0, 0, -135) * Time.deltaTime);
+      if (CatBlocked)
+      {
+        catCounter++;
+      }
     }
 
     if (Input.GetKeyDown(KeyCode.F))
@@ -76,15 +96,22 @@ public class Player : NetworkBehaviour
       CmdUsePickup();
     }
 
-    if (Input.GetKeyDown(KeyCode.R))
+    if (Boosting)
     {
-      CmdGeneratePickup();
+      rgd2d.velocity += (Vector2)transform.up * 3 * Speed * Time.deltaTime;
     }
 
-    if (Input.GetKeyDown(KeyCode.G))
-    {
-      CmdGenerateObstacle();
-    }
+    #region Debug
+    //if (Input.GetKeyDown(KeyCode.R))
+    //{
+    //  CmdGeneratePickup();
+    //}
+
+    //if (Input.GetKeyDown(KeyCode.G))
+    //{
+    //  CmdGenerateObstacle();
+    //}
+    #endregion
   }
 
   #region Debugging
@@ -95,7 +122,7 @@ public class Player : NetworkBehaviour
     {
       return;
     }
-    GameObject temp = Instantiate(Pickup, transform.up * 3 + transform.position, Quaternion.identity);
+    GameObject temp = Instantiate(Pickup, transform.up * 3 + transform.position + new Vector3(0, 0, 100), Quaternion.identity);
     NetworkServer.Spawn(temp);
   }
 
@@ -107,7 +134,7 @@ public class Player : NetworkBehaviour
       return;
     }
 
-    GameObject temp = Instantiate(Obstacle, transform.up * 3 + transform.position, Quaternion.identity);
+    GameObject temp = Instantiate(Obstacle, transform.up * 3 + transform.position + new Vector3(0, 0, 100), Quaternion.identity);
     NetworkServer.Spawn(temp);
   }
   #endregion
@@ -119,12 +146,21 @@ public class Player : NetworkBehaviour
     {
       return;
     }
-    Health -= 100;
-    if (Health <= 0)
+    if (!Blocking)
     {
-      //Player Dies.
-      Destroy(myCamera.gameObject);
-      Destroy(gameObject);
+      Health -= 25;
+      if (Health <= 0)
+      {
+        //Player Dies.
+        //Destroy(myCamera.gameObject);
+        //Destroy(gameObject);
+        dead = true;
+      }
+    }
+    else
+    {
+      Destroy(myShield);
+      Blocking = false;
     }
   }
 
@@ -138,13 +174,26 @@ public class Player : NetworkBehaviour
 
     Destroy(collision);
   }
+
   [Command]
   void CmdGeneratePickupDetails()
   {
-    if (!isServer){
+    if (!isServer)
+    {
       return;
     }
-    myPickup = "Harpoon";
+    switch (Random.Range(0, myPickups.Length))
+    {
+      case (0):
+        myPickup = "Harpoon";
+        break;
+      case (1):
+        myPickup = "Kitten";
+        break;
+      case (2):
+        myPickup = "Shield";
+        break;
+    }
   }
 
   [Command]
@@ -158,10 +207,38 @@ public class Player : NetworkBehaviour
     switch (myPickup)
     {
       case ("Harpoon"):
-        NetworkServer.Spawn(Instantiate(myPickups[0], (transform.up * 3 + transform.position), Quaternion.identity));
+        NetworkServer.Spawn(Instantiate(myPickups[0], (transform.up * 3 + transform.position) + new Vector3(0, 0, 100), transform.rotation));
+        break;
+      case ("Kitten"):
+        NetworkServer.Spawn(Instantiate(myPickups[1], (-transform.up * 3 + transform.position) + new Vector3(0, 0, 100), transform.rotation));
+        break;
+      case ("Shield"):
+        Blocking = true;
+        myShield = Instantiate(myPickups[2], transform, false);
+        NetworkServer.Spawn(myShield);
         break;
     }
     myPickup = "None!";
+  }
+
+  IEnumerator ActivateRockets()
+  {
+    Boosting = true;
+    yield return new WaitForSeconds(2f);
+    Boosting = false;
+  }
+
+  IEnumerator ActivateCat()
+  {
+    CatBlocked = true;
+    //CanvasGroup CatImage = myCamera.transform.Find("Canvas/Cat").GetComponent<CanvasGroup>();
+    //CatImage.alpha = 1;
+    for (int catCounter = 0; catCounter < 240; catCounter++)
+    {
+      yield return null;
+    }
+    //CatImage.alpha = 0;
+    CatBlocked = false;
   }
 
   private void OnCollisionEnter2D(Collision2D collision)
@@ -181,14 +258,46 @@ public class Player : NetworkBehaviour
 
   private void OnTriggerEnter2D(Collider2D collision)
   {
-    if (!isServer){
+    if (!isServer)
+    {
       return;
     }
+
 
     if (collision.transform.tag == "Pickup")
     {
       CmdGeneratePickupDetails();
       CmdDestroyObject(collision.gameObject);
+      return;
+    }
+    else if (collision.transform.tag == "Harpoon")
+    {
+      CmdDestroyObject(collision.gameObject);
+      if (!Boosting && !Blocking)
+      {
+        StartCoroutine(ActivateRockets());
+      }
+      CmdDamage();
+      return;
+    }
+    else if (collision.transform.tag == "Kitten")
+    {
+      CmdDestroyObject(collision.gameObject);
+      if (!CatBlocked && !Blocking)
+      {
+        StartCoroutine(ActivateCat());
+      }
+      CmdDamage();
+      return;
+    }
+    else if (collision.transform.tag == "FakePickup")
+    {
+      CmdDestroyObject(collision.gameObject);
+      if (!Blocking)
+      {
+        CmdDamage();
+      }
+      CmdDamage();
       return;
     }
   }
